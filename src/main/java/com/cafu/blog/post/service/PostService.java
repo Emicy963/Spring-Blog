744c185aaa611febda8e0bf3cde19e5798382c8f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-23 21:38:52 +0100

    Add PostService for do the CRUD information

--- src/main/java/com/cafu/blog/post/service/PostService.java
+++ src/main/java/com/cafu/blog/post/service/PostService.java
@@ -1,4 +1,29 @@
 package com.cafu.blog.post.service;
 
+import com.cafu.blog.post.model.Post;
+import com.cafu.blog.post.repository.PostInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PostService {
+    @Autowired
+    private PostInterface postInterface;
+
+    public PostService(PostInterface postInterface){
+        this.postInterface = postInterface;
+    }
+
+    public Post createPost(Post post){
+        return postInterface.save(post);
+    }
+        
+    public Optional<Post> findPostID(long idPost){return postInterface.findById(idPost);}
+
+    public List<Post> findAllPost(){return postInterface.findAll();}
+
+    public void deletePost(Long idPost){postInterface.deleteById(idPost);}
 }
